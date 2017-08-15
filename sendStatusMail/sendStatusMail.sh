7b09c003e66d0594d61c165e2f0a5fd5464e8d98
BuildTools <unconfigured@null.spigotmc.org>
2017-08-15 23:54:03 +0200

    update

--- sendStatusMail/sendStatusMail.sh
+++ sendStatusMail/sendStatusMail.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+print_header() {
+echo " "
+echo "========================"
+echo "${1}:"
+echo "========================"
+}
+
 mailSub="[$(cat /etc/hostname)] Status"
 mailTo=$1
 tmp=/tmp/sendStatusMail.tmp
@@ -10,22 +17,15 @@ apt-get update >/dev/null
 echo "Uptime: $(uptime)" 		                                 >> $tmp
 echo "Package updates: $(apt-get --just-print upgrade|grep 'upgraded') " >> $tmp
 
-echo ""
-echo "========================"      >> $tmp
-echo "Docker container status:"      >> $tmp
-echo "========================"      >> $tmp
+print_header "Docker container" >> $tmp
 docker container ls >> $tmp
 
-echo ""
-echo "========================"      >> $tmp
-echo "BACKUP: Docker Volumes:"       >> $tmp
-echo "========================"      >> $tmp
-grep "finished:" /opt/backups/docker-volumes/docker-volumes.last.log >> $tmp
+print_header "Running services" >> $tmp
+service --status-all | grep "+" | awk '{print $4}' >> $tmp
 
-echo ""
-echo "========================"      >> $tmp
-echo "BACKUP: Docker Projects:"      >> $tmp
-echo "========================"      >> $tmp
-grep "finished:" /opt/backups/docker-projects/docker-projects.last.log >> $tmp
-
-cat $tmp | mail -s $mailSub $mailTo
+if [ -z $mailTo ]
+then
+ cat $tmp
+else
+ cat $tmp | mail -s $mailSub $mailTo
+fi
